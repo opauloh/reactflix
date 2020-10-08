69be600baf118e9642cadaa6334b4b663e2f340e
opauloh <paulo.scape@gmail.com>
2020-10-08 00:14:19 -0300

    movieCard component

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,21 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
+import { Link } from "react-router-dom";
 
-import styles  from "./MovieCard.module.css";
+import styles from "./MovieCard.module.css";
 
 interface Props {
+  id: number;
   title: string;
   poster: string;
 }
 
-const MovieCard: FC<Props> = ({title, poster}) => (
+const MovieCard: FC<Props> = ({ id, title, poster }) => (
   <div className={styles.card}>
-    <img
-      alt={`Poster for movie ${title}`}
-      className={styles.image}
-      src={`https://image.tmdb.org/t/p/w500/${poster}`}
-    />
-    <p>{title}</p>
+    <Link to={`/movie/${id}`}>
+      <img
+        alt={`Poster for movie ${title}`}
+        className={styles.image}
+        src={poster}
+      />
+      <p>{title}</p>
+    </Link>
   </div>
-)
+);
 
 export default MovieCard;
