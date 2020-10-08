9705f0aa953590d36820fca96f703f2ad0762bea
opauloh <paulo.scape@gmail.com>
2020-10-08 00:30:46 -0300

    update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import * as React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { QueryCache, ReactQueryCacheProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
 import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
+import SearchPage from "./pages/SearchPage";
+import Layout from "./components/Layout";
 
 const queryCache = new QueryCache();
 
 function App() {
   return (
     <ReactQueryCacheProvider queryCache={queryCache}>
-      <HomePage />
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Router>
+        <Switch>
+          <Layout>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/movie/:movieId" component={DetailsPage} />
+            <Route path="/search" component={SearchPage} />
+          </Layout>
+        </Switch>
+      </Router>
     </ReactQueryCacheProvider>
   );
 }
