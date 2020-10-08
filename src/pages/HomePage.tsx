df0d425c085e4bb4bbcffa61e2d2497863575091
opauloh <paulo.scape@gmail.com>
2020-10-08 00:20:27 -0300

    add HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,16 @@
-import * as React from "react";
+import React from "react";
 import { FC } from "react";
 import { useQuery } from "react-query";
 
 import getMovies from "../api/getMovies";
 import MovieList from "../components/MovieList";
-import NavBar from "../components/NavBar";
 
 const HomePage: FC = () => {
   const moviesQuery = useQuery("movies", getMovies, {});
 
   return (
     <>
-      <NavBar />
+      <h2>Home</h2>
       <MovieList moviesList={moviesQuery.data} />
     </>
   );
